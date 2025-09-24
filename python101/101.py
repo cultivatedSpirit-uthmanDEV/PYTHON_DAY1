6bea465934632a97a735b885d432e96666f67ee7
cultivatedSpirit-uthmanDEV <uthman.dev@outlook.com>
2025-09-24 20:37:42 +0100

    day1_lesson
    video_tutorial_1 & 2

---
+++ python101/101.py
@@ -0,0 +1,20 @@
+# VIDEO 1 AND 2
+
+print(3+2) # result equals 5
+
+print(3*20) # 60
+
+print(8 + 2 * 10) # python follows bodmas
+
+print ((8 + 2) * 10)#nums in parenthesis are cal. first
+
+print(18//4) # // tells machine to round the remainder
+
+print(5 ** 3) # multiply 5 by 5 in three times
+
+## VARIABLES
+
+tuna = 5
+print (tuna + 20) # returned 25
+bacon = 15
+print(bacon/tuna)
